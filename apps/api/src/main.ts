90e9e333bad95819af7a234a657b445d64b10134
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-03-23 06:29:05 +0900

    chore: dev command for turbo parallel

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8000);
+  console.info('nest wait on: http://localhost:8000');
 }
 bootstrap();
