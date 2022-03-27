018650de9ae6aec203d45ba645d7bf69799dff8e
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-03-27 13:27:45 +0900

    chore: reduce filter size

--- packages/ui/components/pages/lp/Links.tsx
+++ packages/ui/components/pages/lp/Links.tsx
@@ -8,7 +8,7 @@ export const Links = () => (
     textColor="white"
     spacing={[8, 16, 24]}
     fontSize={['2.6rem', '2.8rem', '3rem']}
-    filter="drop-shadow(0 0 0.75rem white)"
+    filter="drop-shadow(0 0 0.4rem white)"
   >
     <NeonLinkIcon href="https://github.com/ToyB0x" icon={FiGithub} />
     <NeonLinkIcon href="https://twitter.com/ToyB0x_" icon={FiTwitter} />
