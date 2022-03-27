656df8699dd2e742d6ce862bf0aa80afedc0265e
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-03-27 13:08:42 +0900

    feat: setup mdx loader (#14)
    * chore: add next mdx loader deps
    
    * feat: setup next mdx loader

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,9 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withMDX = require('@next/mdx')()
+const withTM = require('next-transpile-modules')(['ui'])
 
-module.exports = withTM({
-  reactStrictMode: true,
-});
+module.exports = withMDX(
+  withTM({
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+    reactStrictMode: true,
+  })
+)
