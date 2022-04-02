1aca4793090d17c2d1ee7357f1b9c80fb16a3187
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-04-02 19:44:52 +0900

    chore: remove ChakraMDXProvider

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,23 +1,20 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ChakraMDXProvider } from '@mdx-lib/chakra'
-import lpTheme from '../lp-theme'
+import { Box, ChakraProvider } from '@chakra-ui/react'
+import theme from '../theme'
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <title>Welcome to blog!</title>
-    </Head>
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Welcome to blog!</title>
+      </Head>
 
-    <ChakraProvider theme={lpTheme}>
-      <ChakraMDXProvider>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </ChakraMDXProvider>
-    </ChakraProvider>
-  </>
-)
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
+}
 
 export default App
