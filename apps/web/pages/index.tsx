3e11c8bac5e05c3de853df4af4ce4a4fa077e6a7
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-04-03 14:15:40 +0900

    chore: remove font loading

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -7,17 +7,6 @@ const Index = () => {
     <>
       <Head>
         <title>ToyB0x</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        {/* eslint-disable-next-line @next/next/google-font-display, @next/next/no-page-custom-font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Neonderthaw&display=block"
-          rel="stylesheet"
-        />
       </Head>
 
       <Center h="100vh" backgroundColor="black">
