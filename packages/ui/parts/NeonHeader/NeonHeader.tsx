7c87d4086fafbd8817e5e3b4151e63f3d1579d71
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-04-03 14:29:04 +0900

    chore: refactor web font loading (#21)
    * chore: add webfontloader
    
    * feat: use webfontloader
    
    * chore: remove font loading

--- packages/ui/parts/NeonHeader/NeonHeader.tsx
+++ packages/ui/parts/NeonHeader/NeonHeader.tsx
@@ -1,21 +1,38 @@
 import { Box, chakra } from '@chakra-ui/react'
 import { blinkKyeFrames } from 'ui'
+import { FC, useEffect, useState } from 'react'
 
-export const NeonHeader: React.FC = () => (
-  <Box
-    color="#fee"
-    userSelect="none"
-    fontFamily="Neonderthaw"
-    fontSize="min(24vw, calc(960px / 6))"
-    textShadow="0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 0.5em #ff4444, 0 0 0.1em #ff4444, 0 10px 3px #000"
-  >
-    Toy
-    <chakra.span animation={`${blinkKyeFrames} linear infinite 2s`}>
-      B
-    </chakra.span>
-    <chakra.span animation={`${blinkKyeFrames} linear infinite 3s`}>
-      o
-    </chakra.span>
-    x
-  </Box>
-)
+const fontFamily = 'Neonderthaw'
+
+export const NeonHeader: FC = () => {
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false)
+
+  useEffect(() => {
+    // See: https://github.com/typekit/webfontloader/issues/383
+    const WebFontLoader = require('webfontloader')
+    WebFontLoader.load({
+      google: { families: [fontFamily] },
+      active: () => setFontLoaded(true),
+    })
+  }, [])
+
+  return (
+    <Box
+      visibility={fontLoaded ? 'visible' : 'hidden'}
+      color="#fee"
+      userSelect="none"
+      fontFamily={fontFamily}
+      fontSize="min(24vw, calc(960px / 6))"
+      textShadow="0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 0.5em #ff4444, 0 0 0.1em #ff4444, 0 10px 3px #000"
+    >
+      Toy
+      <chakra.span animation={`${blinkKyeFrames} linear infinite 2s`}>
+        B
+      </chakra.span>
+      <chakra.span animation={`${blinkKyeFrames} linear infinite 3s`}>
+        o
+      </chakra.span>
+      x
+    </Box>
+  )
+}
