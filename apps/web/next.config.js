155c1f1eec07b8444998d2fa6f4677dc2fc27e2a
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-04-03 20:30:12 +0900

    chore: fix import error

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,9 @@
-const withTM = require('next-transpile-modules')(['ui'])
+const withTM = require('next-transpile-modules')(['ui', '@mdx-lib/chakra'])
 const { withContentlayer } = require('next-contentlayer')
 
 module.exports = withContentlayer(
   withTM({
+    experimental: { esmExternals: 'loose' },
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
     reactStrictMode: true,
   })
