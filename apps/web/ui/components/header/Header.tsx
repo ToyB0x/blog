3d2b9b90c66f3e96edd86c25489b5bee83ba28c3
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-04-17 18:19:48 +0900

    feat: add header

---
+++ apps/web/ui/components/header/Header.tsx
@@ -0,0 +1,58 @@
+import {
+  chakra,
+  Container,
+  HStack,
+  HTMLChakraProps,
+  Spacer,
+  Text,
+} from '@chakra-ui/react'
+import { useEffect, useRef, useState } from 'react'
+import { useViewportScroll } from 'framer-motion'
+import Link from 'next/link'
+import { NeonLinkIcon } from 'ui'
+import { FiGithub, FiInstagram, FiTwitter } from 'react-icons/fi'
+
+export const Header = (props: HTMLChakraProps<'header'>) => {
+  const ref = useRef<HTMLHeadingElement>()
+  const { scrollY } = useViewportScroll()
+  const [y, setY] = useState<number>(0)
+  const { height = 0 } = ref.current?.getBoundingClientRect() ?? {}
+
+  useEffect(() => scrollY.onChange(() => setY(scrollY.get())), [scrollY])
+
+  return (
+    <chakra.header
+      pos="sticky"
+      top="0"
+      zIndex="3"
+      bg="white"
+      shadow={y > height ? 'sm' : undefined}
+      {...props}
+    >
+      <Container>
+        <HStack h={16}>
+          <Text fontSize="xl" fontWeight="semibold" letterSpacing={2}>
+            <Link href="/">
+              <a>
+                ToyB
+                <Text as="span" color="blue.400">
+                  0
+                </Text>
+                x
+              </a>
+            </Link>
+          </Text>
+          <Spacer />
+          <HStack fontSize="2xl" spacing={4}>
+            <NeonLinkIcon href="https://twitter.com/ToyB0x_" icon={FiTwitter} />
+            <NeonLinkIcon
+              href="https://www.instagram.com/toyb0x_/"
+              icon={FiInstagram}
+            />
+            <NeonLinkIcon href="https://github.com/ToyB0x" icon={FiGithub} />
+          </HStack>
+        </HStack>
+      </Container>
+    </chakra.header>
+  )
+}
