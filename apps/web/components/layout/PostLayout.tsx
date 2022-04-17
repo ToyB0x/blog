8f47b4969f4ba9bc1eeb65b580b6722370838757
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-04-17 17:59:25 +0900

    feat: add PostLayout

---
+++ apps/web/components/layout/PostLayout.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import { Box, Heading, Image, Stack } from '@chakra-ui/react'
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import { format, parseISO } from 'date-fns'
+import { MDXComponents } from '@mdx-lib/chakra'
+import { Post } from 'contentlayer/generated'
+import { FC } from 'react'
+
+type Props = {
+  post: Post
+}
+
+export const PostLayout: FC<Props> = ({ post }) => {
+  const MDXComponent = useMDXComponent(post.body.code)
+
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+
+      <Stack spacing={2}>
+        <Box as="time" fontSize="sm" color="gray.600" dateTime={post.date}>
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </Box>
+
+        <Heading isTruncated>{post.title}</Heading>
+
+        {post.photo && (
+          <Image src={post.photo} borderRadius="xl" alt="post header image" />
+        )}
+      </Stack>
+
+      <MDXComponent components={MDXComponents} />
+    </>
+  )
+}
