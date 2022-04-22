c7b4acef2cffb9b641f24926e64d544d9dd3b1f3
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2022-04-22 09:06:02 +0900

    chore: add overflow-y setting

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
+import '../styles/global.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
