fc1463d45fe80ca343379eac829b67f51b12243a
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-02-19 09:21:25 +0900

    chore: remove unused package

--- apps/api/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
-  console.info('nest wait on: http://localhost:8080');
-}
-bootstrap();
