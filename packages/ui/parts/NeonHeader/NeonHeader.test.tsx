597ce05dfb2e778811afe878084e560f6c2b3be9
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-10-21 12:59:04 +0900

    remove parts

--- packages/ui/parts/NeonHeader/NeonHeader.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import { NeonHeader } from './NeonHeader'
-
-describe('Header', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<NeonHeader />)
-    expect(baseElement).toBeTruthy()
-  })
-})
--- packages/ui/parts/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './NeonHeader'
-export * from './NeonLink'
