f8b787d14435b3e802daf93eb30827f0a29df959
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-10-26 13:19:04 +0900

    update next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 const { withContentlayer } = require('next-contentlayer')
 
 module.exports = withContentlayer({
+  output: 'export',
   transpilePackages: ['ui', 'mdx-lib'],
   experimental: { esmExternals: 'loose' },
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
