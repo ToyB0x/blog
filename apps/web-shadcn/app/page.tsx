518a439b689b8cc92e9692e1581c550ef4a11e69
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-11-09 12:36:41 +0900

    feat: add new blog design with shadcn (#146)
    * feat: add posts
    
    * add mdx-components.tsx
    
    * add post page
    
    * add posts page
    
    * add css
    
    * add layout page / analytics
    
    * add components
    
    * add index page
    
    * add about page
    
    * add config files
    
    * add package json
    
    * add images
    
    * update lock file

---
+++ apps/web-shadcn/app/page.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import compareDesc from 'date-fns/compareDesc'
+import { allPosts } from '@/.contentlayer/generated'
+
+export default function Home() {
+  const sortedPosts = allPosts.sort((a, b) => {
+    return compareDesc(new Date(a.date), new Date(b.date))
+  })
+
+  return (
+    <div className="prose dark:prose-invert">
+      {sortedPosts.map((post) => (
+        <article key={post._id}>
+          <Link href={post.slug}>
+            <h2>{post.title}</h2>
+          </Link>
+          {post.description && <p>{post.description}</p>}
+        </article>
+      ))}
+    </div>
+  )
+}
