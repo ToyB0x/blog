db61bd7c6996e50a0a8d0519a61ef2035aafd14b
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-11-16 10:06:26 +0900

    chore: update menu

--- apps/web-shadcn/app/layout.tsx
+++ apps/web-shadcn/app/layout.tsx
@@ -27,11 +27,30 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <div className="max-w-2xl mx-auto py-10 px-4">
             <header>
               <div className="flex items-center justify-between">
-                <ModeToggle />
-                <nav className="ml-auto text-sm font-medium space-x-6">
-                  <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
-                </nav>
+                {/* left items */}
+                <div className="text-3xl font-medium">
+                  <Link href="/">
+                    ToyB
+                    <span className="text-blue-300">0</span>x{/*<Image*/}
+                    {/*  src="/avatar/avatar.png"*/}
+                    {/*  width={38}*/}
+                    {/*  height={38}*/}
+                    {/*  alt="Picture of the author"*/}
+                    {/*  className="inline"*/}
+                    {/*/>*/}
+                  </Link>
+                </div>
+
+                {/* right items */}
+                <div className="ml-auto space-x-6 flex">
+                  <nav className="font-medium space-x-6">
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                  </nav>
+                  <div>
+                    <ModeToggle />
+                  </div>
+                </div>
               </div>
             </header>
             <main>{children}</main>
