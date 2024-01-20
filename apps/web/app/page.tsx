c08fd2465f7efeaf912eb855d7833047806226a2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-01-20 09:29:37 +0900

    fix: import

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import compareDesc from 'date-fns/compareDesc'
+import { compareDesc } from 'date-fns/compareDesc'
 import { allPosts } from '@/.contentlayer/generated'
 
 export default function Home() {
