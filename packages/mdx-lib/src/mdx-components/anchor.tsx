83cc183bce7116dcda49dfa358737119daa0c879
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-01-20 09:58:49 +0900

    chore: remove package

--- packages/mdx-lib/src/mdx-components/anchor.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import { chakra, HTMLChakraProps } from '@chakra-ui/react'
-
-export const Anchor = (props: HTMLChakraProps<'a'>) => (
-  <chakra.a apply="mdx.a" {...props} />
-)
--- packages/mdx-lib/src/mdx-components/codeblock/code-container.tsx
+++
@@ -1,8 +0,0 @@
-import { Box, BoxProps } from '@chakra-ui/react'
-import React from 'react'
-
-function CodeContainer(props: BoxProps) {
-  return <Box rounded="8px" my="8" overflow="hidden" bg="#011627" {...props} />
-}
-
-export default CodeContainer
