485e043a42721b3adbeb262f73af473046023f33
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-10-02 08:43:13 +0900

    test: rename test file (#241)
    * test: rename test file
    
    * update pin node / pnpm
    
    * fix node

--- tests/page-open.spec.ts
+++ tests/page-open.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('test', async ({ page }) => {
+test('can open page correctly', async ({ page }) => {
 	await page.goto('http://localhost:4321/')
 	await expect(page.getByRole('heading', { name: 'ToyB0x' })).toBeVisible()
 
