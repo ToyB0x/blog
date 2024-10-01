e57481d664fc336dcfbcbb2d8a0e0ace3880ac27
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-10-02 05:37:41 +0900

    feat: e2e test (#240)
    * add deps
    
    * chore: use json5
    
    * add ci
    
    * add e2d
    
    * add config
    
    * fix typo
    
    * fix

---
+++ tests/example.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test'
+
+test('test', async ({ page }) => {
+	await page.goto('http://localhost:4321/')
+	await expect(page.getByRole('heading', { name: 'ToyB0x' })).toBeVisible()
+
+	await page.getByText('Blog').click()
+	await expect(page.getByRole('heading', { name: 'Blog' })).toBeVisible()
+
+	await page.getByText('Tools').click()
+	await expect(page.getByRole('heading', { name: 'Tools' })).toBeVisible()
+})
