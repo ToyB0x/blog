65cc274bbb4f7ea224a719faba89c744396f932c
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2024-10-03 06:51:54 +0900

    run fix

--- src/utils/remarkReadingTime.ts
+++ src/utils/remarkReadingTime.ts
@@ -1,10 +1,10 @@
-import { toString } from 'mdast-util-to-string'
+import { toString as mdAstTostring } from 'mdast-util-to-string'
 import getReadingTime from 'reading-time'
 
 export function remarkReadingTime() {
 	// @ts-expect-error:next-line
 	return (tree, { data }) => {
-		const textOnPage = toString(tree)
+		const textOnPage = mdAstTostring(tree)
 		const readingTime = getReadingTime(textOnPage)
 		// readingTime.text will give us minutes read as a friendly string,
 		// i.e. "3 min read"
