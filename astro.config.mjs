334c41646f21f2e0d6317b6b8253ff8eedb773e0
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-06-30 05:57:47 +0900

    deps: bump biome (#323)
    * feat: update biome configuration to version 2.0.0
    
    * feat: reorganize imports across multiple components
    
    * feat: enable version control system in biome configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config'
 import expressiveCode from 'astro-expressive-code'
 import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
 import rehypeExternalLinks from 'rehype-external-links'
 import remarkUnwrapImages from 'remark-unwrap-images'
 import { expressiveCodeOptions } from './src/site.config'
 import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
